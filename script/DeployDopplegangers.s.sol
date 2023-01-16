0621927b47ef0347b0c98dee0921b7c68c4b71dd
James Wenzel <jameswenzel@users.noreply.github.com>
2023-01-15 16:56:51 -0800

    update deploy script

--- script/DeployDopplegangers.s.sol
+++ script/DeployDopplegangers.s.sol
@@ -5,10 +5,11 @@ import { BaseCreate2Script, console2 } from "create2-scripts/BaseCreate2Script.s
 import { IOwnedMirror } from "../src/interfaces/IOwnedMirror.sol";
 import { Hello } from "../test/helpers/Hello.sol";
 import { World } from "../test/helpers/World.sol";
-import { HuffDeployer } from "foundry-huff/HuffDeployer.sol";
+import { HuffConfig } from "foundry-huff/HuffConfig.sol";
 
 contract DeployDopplegangers is BaseCreate2Script {
     function run() public {
+        setUp();
         console2.log(vm.toString(deployer));
         runOnNetworks(this.execute, vm.envString("NETWORKS", ","));
     }
@@ -32,20 +33,23 @@ contract DeployDopplegangers is BaseCreate2Script {
         return _immutableCreate2IfNotDeployed(
             deployer,
             bytes32(0),
-            HuffDeployer.config().with_args(abi.encode(address(0), deployer)).creationCodeWithArgs("OwnedMirror")
+            (new HuffConfig()).with_args(abi.encode(address(0), deployer)).creationCodeWithArgs("OwnedMirror")
         );
     }
 
     function configureMirror(IOwnedMirror mirror, bytes memory code) internal {
+        console2.log("configuring mirror");
         address created = _create2IfNotDeployed(deployer, bytes32(0), code);
+        vm.broadcast(deployer);
         mirror.setMirrored(created);
     }
 
     function deployDoppleganger(IOwnedMirror mirror) internal returns (address) {
+        console2.log("deploying doppleganger");
         return _immutableCreate2IfNotDeployed(
             deployer,
-            bytes32(0),
-            HuffDeployer.config().with_args(abi.encodePacked(address(mirror))).creationCodeWithArgs("Doppleganger")
+            bytes32(uint256(1)),
+            (new HuffConfig()).with_args(abi.encodePacked(address(mirror))).creationCodeWithArgs("Doppleganger")
         );
     }
 }
